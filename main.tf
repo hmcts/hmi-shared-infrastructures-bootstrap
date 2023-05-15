28c39b1581672135b8a6e8c3bb6e3e621a363155
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-05-15 11:53:18 +0100

    Updated rg

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 locals {
-  prefix = "${var.product}-sharedinfra"
+  prefix = "${var.product}-bootstrap"
   resource_group_name  = "${local.prefix}-${var.env}-rg"
 }
 
-# Needs to be a resource when current repo is deleted.
-data "azurerm_resource_group" "rg" {
-  name = local.resource_group_name
+resource "azurerm_resource_group" "rg" {
+  name     = local.resource_group_name
+  location = var.location
+  tags     = var.common_tags
 }
 
 data "azurerm_client_config" "current" {}
