ea0831da3efc4dc22b3f7f348fd404e9de77ec56
52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
2024-07-03 17:45:04 +0000

    Updating Terraform Formatting

--- key-vaults.tf
+++ key-vaults.tf
@@ -3,13 +3,13 @@ locals {
 }
 
 module "kv_hmi" {
-  source                      = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                        = local.key_vault_name
-  product                     = var.product
-  env                         = var.env
-  object_id                   = var.jenkins_identity_object_id
-  resource_group_name         = resource.azurerm_resource_group.rg.name
-  product_group_name          = var.active_directory_group
-  common_tags                 = var.common_tags
-  create_managed_identity     = true
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                    = local.key_vault_name
+  product                 = var.product
+  env                     = var.env
+  object_id               = var.jenkins_identity_object_id
+  resource_group_name     = resource.azurerm_resource_group.rg.name
+  product_group_name      = var.active_directory_group
+  common_tags             = var.common_tags
+  create_managed_identity = true
 }
