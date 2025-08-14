9b97092abab36e2b4d9aae4e0182ae8f60f3fc01
Christopher Stacey <christopher.stacey@justice.gov.uk>
2025-08-14 11:16:39 +0100

    PUB-2878 - Added in MI

--- key-vaults.tf
+++ key-vaults.tf
@@ -2,14 +2,20 @@ locals {
   key_vault_name = "${var.product}-bootstrap-kv-${var.env}"
 }
 
+data "azurerm_user_assigned_identity" "hmi-mi" {
+ name                = "${var.product}-${var.env}-mi"
+ resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 module "kv_hmi" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                    = local.key_vault_name
-  product                 = var.product
-  env                     = var.env
-  object_id               = var.jenkins_identity_object_id
-  resource_group_name     = resource.azurerm_resource_group.rg.name
-  product_group_name      = var.active_directory_group
-  common_tags             = var.common_tags
-  create_managed_identity = false
+  source                      = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                        = local.key_vault_name
+  product                     = var.product
+  env                         = var.env
+  object_id                   = var.jenkins_identity_object_id
+  resource_group_name         = resource.azurerm_resource_group.rg.name
+  product_group_name          = var.active_directory_group
+  common_tags                 = var.common_tags
+  create_managed_identity     = false
+  managed_identity_object_ids = [data.azurerm_user_assigned_identity.hmi-mi.principal_id]
 }
