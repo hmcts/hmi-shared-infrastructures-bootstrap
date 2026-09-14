0bf98101bdb10bd41dd235ae09e19ac2020dd777
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-14 16:23:16 +0100

    use jenkins_object_id

--- key-vaults.tf
+++ key-vaults.tf
@@ -12,7 +12,7 @@ module "kv_hmi" {
   name                        = local.key_vault_name
   product                     = var.product
   env                         = var.env
-  object_id                   = var.jenkins_identity_object_id
+  jenkins_object_id           = var.jenkins_identity_object_id
   resource_group_name         = resource.azurerm_resource_group.rg.name
   product_group_name          = var.active_directory_group
   common_tags                 = var.common_tags
